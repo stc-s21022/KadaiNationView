0c8f93392bc63b4da718acd38fa8fa0906a13542
s21022 <s21022@suntech.jp>
2022-07-05 12:04:13 +0900

    JavaProgram add

--- app/src/main/java/jp/suntech/s21022/kadainationview/MainActivity.java
+++ app/src/main/java/jp/suntech/s21022/kadainationview/MainActivity.java
@@ -3,6 +3,10 @@ package jp.suntech.s21022.kadainationview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView cList = findViewById(R.id.countrylist);
+        cList.setOnItemClickListener(new ListItemClickLister());
+    }
+
+    private class ListItemClickLister implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            TextView c_label = findViewById(R.id.countrylabel);
+            TextView select = (TextView) view;
+            String select_label = select.getText().toString();
+
+            c_label.setText(select_label);
+
+        }
     }
 }
